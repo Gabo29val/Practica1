98776aa757eac9a64beceb87f394b78ba58ce514
Gabo29val <ga_al5@hotmail.com>
2020-01-28 13:00:14 -0500

    agregando labo4

---
+++ Labo04_ObjetosClases/src/Labo04_ObjetosClases/Square.java
@@ -0,0 +1,30 @@
+package Labo04_ObjetosClases;
+
+public class Square {
+	
+	int width;
+	int heigth;
+	float area;
+	
+	public int getWidth() {
+		return width;
+	}
+	public void setWidth(int width) {
+		this.width = width;
+	}
+	public int getHeigth() {
+		return heigth;
+	}
+	public void setHeigth(int heigth) {
+		this.heigth = heigth;
+	}
+	public float getArea() {
+		return area;
+	}
+	public void setArea(float area) {
+		this.area = area;
+	}
+	
+	
+
+}
